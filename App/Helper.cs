c8d80ff456544a471e1e5506b9b48f80cba951f3
Fazan4ik <jagannath2006@outlook.com>
2023-11-30 16:09:46 +0200

    123

--- App/Helper.cs
+++ App/Helper.cs
@@ -8,6 +8,24 @@ namespace App
 {
     public class Helper
     {
+        public String CombineUrl(String part1, String part2)
+        {
+            if (!part1.StartsWith('/')) part1 = '/' + part1;
+            if (part1.EndsWith("/")) part1 = part1[..^1];
+
+            if (!part2.StartsWith('/')) part2 = '/' + part2;
+            if (part2.EndsWith("/")) part2 = part2[..^1];
+            if (part1.EndsWith("/.."))
+            {
+                int lastSlashIndex = part1.LastIndexOf('/', part1.Length - 4);
+                if (lastSlashIndex >= 0)
+                {
+                    part1 = part1.Substring(0, lastSlashIndex);
+                }
+            }
+
+            return $"{part1}{part2}";
+        }
         public String Ellipsis(String input, int len)
         {
             //return (len == 5) ? "He..." : "Hel...";
--- UniTest/UnitTest1.cs
+++ UniTest/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace UniTest
         public void EllipsisTest()
         {
             Helper helper = new();
-            Assert.IsNotNull(helper,"new helper() should not be null");
+            Assert.IsNotNull(helper, "new helper() should not be null");
             Assert.AreEqual("He...", helper.Ellipsis("Hello, World", 5));
             Assert.AreEqual("Hel...", helper.Ellipsis("Hello, World", 6));
         }
@@ -27,6 +27,27 @@ namespace UniTest
             Assert.IsNotNull(helper, "new helper() should not be null");
             Assert.AreEqual("Hello, World.", helper.Finalize("Hello, World"));
             Assert.AreEqual("Hello, World.", helper.Finalize("Hello, World."));
+        }
+        [TestMethod]
+        public void CombineUrlTest()
+        {
+            Helper helper = new();
+            Dictionary<String[], String> testCases = new()
+            {
+                {new[]{"/home","index" }, "/home/index"},
+                {new[]{"/home/","/index" } ,"/home/index"},
+                {new[]{"home/","index" }, "/home/index"},
+                {new[]{"/home///","index" }, "/home/index"},
+                {new[]{"////home/","/index" } ,"/home/index"},
+                {new[]{"home/","////index" }, "/home/index"},
+                {new[]{"/shop/cart/user/../123", "" }, "/shop/cart/123"}
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    helper.CombineUrl(testCase.Key[0], testCase.Key[1]), $"{testCase.Value} -- {testCase.Key[0]} + {testCase.Key[1]}");
+            }
         }
     }
 }
